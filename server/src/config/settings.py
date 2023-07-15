eb95f9ad70dd92b28dcd2a04a70faa2c5749ddf2
AlekseyAmp <thientaovv@gmail.com>
2023-07-15 11:33:21 +0500

    Add .env

--- server/src/config/settings.py
+++ server/src/config/settings.py
@@ -7,7 +7,8 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    REDIS_HOSTNAME: str = os.environ["POSTGRESQL_HOSTNAME"]
-    REDIS_PORT: str = os.environ["POSTGRESQL_USERNAME"]
+    REDIS_HOSTNAME: str = os.environ["REDIS_HOSTNAME"]
+    REDIS_PORT: str = os.environ["REDIS_PORT"]
+
 
 settings = Settings()
