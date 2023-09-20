3732236818e430247c411e275e38a41b0cffc83e
AlekseyAmp <thientaovv@gmail.com>
2023-09-21 01:33:07 +0500

    Added initial content to the main page

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
+import styles from './Main.module.scss';
+
+import Textarea from '../../components/UI/Textarea/Textarea';
+import Button from '../../components/UI/Button/Button';
+
 function Main() {
   return (
-    <div>Main</div>
+    <div className={styles.mainContent}>
+      <h1 className={'title'}>Делитесь информацией безопасно.</h1>
+      <div className={styles.mainContentBox}>
+        <p className={'gray-text'}>
+          Привет! Я создаю секретные ссылки.
+        </p>
+        <p className={'dark-gray-text'}>
+          Просто введите текст, и мы сгенерируем ссылку, которая самоуничтожится после просмотра или через указанное Вами время.
+        </p>
+        <Textarea />
+      </div>
+    </div>
   )
 }
 
-export default Main
+export default Main;
