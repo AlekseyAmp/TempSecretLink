59062b263c4d8b887a85c84e8cfb8625d133f778
AlekseyAmp <thientaovv@gmail.com>
2023-09-21 23:41:48 +0500

    Added a constant with expiration dates that can be selected by the user in Select

---
+++ client/src/constants/expire_dates.js
@@ -0,0 +1,18 @@
+const expire_dates = [
+  { label: '1 минута', value: 60 },
+  { label: '5 минут', value: 300 },
+  { label: '30 минут', value: 1800 },
+  { label: '1 час', value: 3600 },
+  { label: '3 часа', value: 10800 },
+  { label: '6 часов', value: 21600 },
+  { label: '12 часов', value: 43200 },
+  { label: '1 день', value: 86400 },
+  { label: '2 дня', value: 172800 },
+  { label: '3 дня', value: 259200 },
+  { label: '4 дня', value: 345600 },
+  { label: '5 дней', value: 432000 },
+  { label: '6 дней', value: 518400 },
+  { label: '7 дней', value: 604800 }
+];
+
+export { expire_dates };
